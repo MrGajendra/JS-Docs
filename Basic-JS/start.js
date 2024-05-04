9356c98d530486cb2099a1ad8a808ec309f8c741
Gajendra Kumar <123275191+MrGajendra@users.noreply.github.com>
2024-05-04 13:52:43 +0530

    Update start.js

--- Basic-JS/start.js
+++ Basic-JS/start.js
@@ -1,1 +1,11 @@
-// 
+// by using slash twice we can comment in out code base bsaically commnet are neglected by js engine 
+
+/* this is another way of 
+writing 
+comment */
+
+//so now you know basic lets start something crazy 
+
+// in this doc we will lern by creating mini projects
+
+    
